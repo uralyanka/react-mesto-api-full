25d59ad1dc717ff86cf136201b85def17769ad0c
Yana Evstegneeva <yanka.evste@gmail.com>
2022-10-14 21:32:32 +0500

    PR 15: change cors

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -128,6 +128,8 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
+        sameSite: 'none',
+        secure: true,
       });
       res.send({ token });
     })
